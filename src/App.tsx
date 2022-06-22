2cda085c29e1aa0e52349d968196511bf56b37e1
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-06-22 00:28:20 -0300

    feat: install styled components as project css-in-js library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 64px;
   color: #8257e6;
-`
+`;
 
 export function App() {
   return (
