bb3d76c8c2f944482a90cdbfbe5d05472390955d
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-06-22 01:26:37 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <div>App works!</div>
+      <GlobalStyle />
     </div>
   );
 }
