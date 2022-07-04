f1c90e21633d42cf5edbd9d441ad4c29f7321aeb
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-07-04 20:28:53 -0300

    feat: create new transaction modal structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,8 +13,29 @@ export function NewTransactionModal(props: NewTransactionModalProps) {
     <Modal
       isOpen={props.isOpen}
       onRequestClose={props.onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        
+        <input
+          placeholder="Título"
+        />
+        
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   );
 }
