71332cc98dcea248fb5c088ab799ffc340f73018
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-07-14 00:46:51 -0300

    feat: moving create transaction logic to context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 
 import Modal from "react-modal";
 
@@ -7,6 +7,7 @@ import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { TransactionsContext } from "../../TransactionsContext";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,22 +15,21 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal(props: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
-  const [type, setType] = useState('deposit');
+  const [type, setType] = useState<'deposit' | 'withdraw'>('deposit');
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-
-    const data = {
+    createTransaction({
       title,
-      value,
+      amount,
       category,
       type
-    }
-
-    api.post('/transactions', data)
+    });
   }
 
   Modal.setAppElement("#root");
@@ -60,8 +60,8 @@ export function NewTransactionModal(props: NewTransactionModalProps) {
         <input
           type="number"
           placeholder="Valor"
-          value={!!value ? value : ''}
-          onChange={(event) => setValue(Number(event.target.value))}
+          value={!!amount ? amount : ''}
+          onChange={(event) => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
